0fb2870f3d3175a3057b785bc2ef17062b6b550b
kristinaaleee <kristinalee2000@hotmail.com>
2025-06-23 21:43:22 -0400

    Completed linked list class with extra methods

---
+++ linked-list.js
@@ -0,0 +1,141 @@
+class LinkedList{
+    // create empty list
+    constructor() {
+        this.head = null
+    }
+    append(value){
+        const newNode = new Node (value) 
+        //if empty list
+        if (this.head === null){
+            this.head = newNode;
+            return
+        }
+        let currentNode = this.head;
+        while (currentNode.next != null){
+            currentNode = currentNode.next;
+        }
+        // current is now last value before node
+        currentNode.next = newNode;
+    }
+    prepend(value){
+        const newNode = new Node(value);
+        //new node point to old list start
+        newNode.next = this.head;
+        //list start with new node
+        this.head = newNode;
+    }
+    toString(){
+        let currentNode = this.head;
+        let finalString = '';
+        // step through each node
+        while (currentNode){
+            finalString += `( ${currentNode.value} ) -> `
+            currentNode = currentNode.next;
+        }
+        finalString += 'null'
+        return finalString;
+    }
+    size(){
+        let currentNode = this.head;
+        let listLength = 0;
+        while (currentNode){
+            listLength++;
+            currentNode = currentNode.next;
+        }
+        return listLength;
+    }
+    //error when naming method head
+    start(){
+        let currentNode = this.head;
+        return currentNode.value;
+    }
+    tail(){
+       let currentNode = this.head;
+       while (currentNode.next != null){
+        currentNode = currentNode.next;
+       } 
+       return currentNode.value;
+    }
+    at(index){
+        let currentNode = this.head;
+        let currentIndex = 0;
+        while (currentIndex != index){
+            currentNode = currentNode.next;
+            currentIndex++
+        }
+        return currentNode.value;
+    }
+    pop(){
+        let currentNode = this.head;
+        while (currentNode.next.next != null){
+            currentNode = currentNode.next
+        }
+        currentNode.next = null;
+    }
+    contains(value){
+        let currentNode = this.head;
+        while (currentNode){
+            if (currentNode.value === value){
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+    find(value){
+        let currentNode = this.head;
+        let currentIndex = 0;
+        while (currentNode){
+            if (currentNode.value === value){
+                return currentIndex;
+            }
+            currentNode = currentNode.next;
+            currentIndex++
+        }
+        return null;
+    }
+    insertAt(value, index){
+        let currentNode = this.head;
+        let currentIndex = 0;
+        let newNode = new Node (value);
+        while (currentIndex != index - 1){
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+    }
+    removeAt(index){
+        let currentNode = this.head;
+        let currentIndex = 0;
+        while (currentIndex != index - 1){
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        currentNode.next = currentNode.next.next;
+    }
+}
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+const list = new LinkedList();
+list.append('dog');
+list.append('hedgehog');
+list.append('end');
+list.append('cat');
+list.prepend('first');
+list.pop();
+list.insertAt('bunny', 1)
+list.removeAt(2);
+console.log(list.size());
+console.log(list.toString());
+console.log(list.start());
+console.log(list.tail());
+console.log(list.at(2))
+console.log(list.contains('bunny'));
+console.log(list.find('bird'));
